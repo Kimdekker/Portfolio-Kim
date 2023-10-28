56b0445126022645520a68cdd06fbe85110077c0
Kim Dekker <kim11dekekr@gmail.com>
2023-10-28 17:46:36 +0200

    nav menu + index start + animations

--- scripts/script.js
+++ scripts/script.js
@@ -33,21 +33,23 @@ function motionYesNo() {
     iconMotion.classList.toggle('upsidedown');
 }
 
-// var menuBtn = document.querySelector('header button');
 
-// menuBtn.onclick = foldMenu
+//Menu openen
+var menuBtn = document.querySelector('header nav:nth-of-type(2) button');
 
-// function foldMenu() {
-//   var menuBtn = document.querySelector('header nav');
-//   var lineOne = document.querySelector('header button span:nth-of-type(1)');
-//   var lineTwo = document.querySelector('header button span:nth-of-type(2)');
-//   var lineThree = document.querySelector('header button span:nth-of-type(3)');
+menuBtn.onclick = foldMenu
 
-//   menuBtn.classList.toggle('showmenu');
-//   lineOne.classList.toggle('rotateplus');
-//   lineTwo.classList.toggle('leave');
-//   lineThree.classList.toggle('rotatemin');
-// }
+function foldMenu() {
+  var foldedMenu = document.querySelector('header nav:last-of-type');
+  var lineOne = document.querySelector('header nav:nth-of-type(2) button span:nth-of-type(1)');
+  var lineTwo = document.querySelector('header nav:nth-of-type(2) button span:nth-of-type(2)');
+  var lineThree = document.querySelector('header nav:nth-of-type(2) button span:nth-of-type(3)');
+
+  foldedMenu.classList.toggle('showmenu');
+  lineOne.classList.toggle('rotateplus');
+  lineTwo.classList.toggle('leave');
+  lineThree.classList.toggle('rotatemin');
+}
 
 
 
